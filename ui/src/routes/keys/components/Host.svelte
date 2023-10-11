69ba032137397d63946599e4c46f8fb6a38fefaf
lanmower <almagestfraternite@gmail.com>
2023-10-11 15:57:19 +0200

    first working store replication

--- ui/src/routes/keys/components/Host.svelte
+++ ui/src/routes/keys/components/Host.svelte
@@ -1,10 +1,47 @@
 <script>
+
+    import { Table, tableMapperValues } from '@skeletonlabs/skeleton'
+    let newKey, newValue
     let nodes = []
     let nodename
+    let selectedKey 
     export let keyName = ""
     export let rootKey
     export let host
-    
+
+    let sourceData = {}
+    const handleSelectedKey = (inp) => {
+        selectedKey=inp.detail[0]
+    }
+    const refreshTable = ()=>{
+        const mapper = Object.keys(sourceData).map(key=>{
+                return {key, value:sourceData[key]}
+            })
+            console.log(mapper)
+        let tableSimple = {
+            head: ['Environment Var', 'Value'],
+            body: tableMapperValues(mapper, ['key', 'value']),
+            meta: tableMapperValues(mapper, ['key', 'value']),
+            foot: ['Total', `${sourceData.length||'0'}`]
+        }
+        return tableSimple;
+    }
+    let tableSimple = refreshTable();
+    const saveValue = ()=>{
+        const output = {...sourceData}
+        output[newKey] = newValue
+        sourceData = output
+        console.log(sourceData)
+        tableSimple = refreshTable()
+    }
+    const deleteValue = ()=>{
+        console.log('test')
+        const output = {...sourceData}
+        delete output[selectedKey]
+        sourceData = output
+        console.log(sourceData)
+        tableSimple = refreshTable()
+    }
     const getNodes =  async (host) => {
         const nodesFetch = await fetch(`http://localhost:3011/vault/getNodes`, {
             method: "POST",
@@ -37,10 +74,10 @@
             }
         })
         const sub = await subFetch.json();
-        console.log(sub, host)
+        console.log({sourceData})
         const nodeFetch = await fetch(`http://localhost:3011/vault/startNode/`+nodename, {
             method: "POST",
-            body: JSON.stringify({hostKey:{publicKey:host}, sub}),
+            body: JSON.stringify({hostKey:{publicKey:host}, sub, env:sourceData}),
             headers: {
                 "Content-Type": "application/json",
             }
@@ -50,7 +87,19 @@
 </script>
 
 <div class="variant-filled-surface rounded-b-lg p-4">
-    <h1 class="m-4 text-lg ">Start node: </h1>
+    <input class="input max-w-sm" placeholder="env" bind:value={newKey}/>
+    <input class="input max-w-sm" placeholder="value" bind:value={newValue}/>
+    <button
+        class="bg-green-500 text-white px-4 py-2 rounded-md hover:bg-green-600 m-2"
+        on:click={saveValue}>Save</button>
+    {#key sourceData}
+    {#if Object.keys(sourceData).length}
+    <button
+        class="bg-red-500 text-white px-4 py-2 rounded-md hover:bg-green-600 m-2"
+        on:click={deleteValue}>Delete</button>
+    {/if}
+    {/key}
+    <Table source={tableSimple} interactive={true} on:selected={handleSelectedKey}/>
     <input
     type="text"
     bind:value={nodename}
@@ -61,10 +110,12 @@
     {#if nodes.length}
     <h1 class="max-w-xl text-lg m-4">Running:</h1>
     {#each nodes as node, i}
-        <button  type="button" class="btn variant-filled m-4">
-            {node.name}
-            <button type="button" class="btn-icon variant-filled-surface ml-2">X</button>
-        </button>
+        <div class="varian-ghost-surface">
+            <button  type="button" class="btn variant-filled m-4">
+                {node.name}
+                <button type="button" class="btn-icon variant-filled-surface ml-2">X</button>
+            </button>
+        </div>
     {/each}
     {/if}
 
--- ui/src/routes/store/Store.svelte
+++ ui/src/routes/store/Store.svelte
@@ -2,21 +2,33 @@
     import { Table, tableMapperValues } from '@skeletonlabs/skeleton'
     let newKey, newValue
     export let name
+    export let publicKey
 
-    const sourceData = {}
-
-    const mapper = Object.keys(sourceData).map(key=>{return{key, value:sourceData[key]}});
-    const tableSimple = {
-        head: ['Name', 'Value'],
-        body: tableMapperValues(mapper, ['key', 'value']),
-        meta: tableMapperValues(mapper, ['key', 'value']),
-        foot: ['Total', `<code class="code">${sourceData.length}</code>`]
+    let sourceData = {};
+    const refreshTable = ()=>{
+        const mapper = Object.keys(sourceData).map(key=>{return{key, value:sourceData[key]}});
+        return {
+            head: ['Name', 'Value'],
+            body: tableMapperValues(mapper, ['key', 'value']),
+            meta: tableMapperValues(mapper, ['key', 'value']),
+            foot: ['Total', `${Object.keys(sourceData).length}`]
+        }
     }
+    let tableSimple = refreshTable();
+    (async()=>{
+        const url = `http://localhost:3011/store/loadAll/${name}`
+        const fetched = await fetch(url, { method: "GET" })
+        const json = await fetched.json();
+        sourceData = {...json.values}
+        tableSimple = refreshTable()
+    })()
+
     const saveValue = ()=>{
         
     }
 </script>
 <div>
+    <h1 class="text-lg">Replication key: </h1><div>{publicKey}</div>
     <input class="input max-w-md" placeholder="key" bind:value={newKey}/>
     <input class="input max-w-lg" placeholder="value" bind:value={newValue}/>
     <button
